332460d0902e504dad27501b3d2b7568da5bdbcc
chewkei_merck <keith.zikai.chew@msd.com>
2025-10-17 14:12:09 +0800

    use React Router navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LandingPage } from './components/LandingPage';
 
 const App: React.FC = () => {
+  const handleGetStarted = () => {
+    // TODO: Navigate to auth page when implemented
+    console.log('Navigate to auth page');
+  };
+
   return (
-    <div className="size-full">
-      <LandingPage onGetStarted={() => {}} />
-    </div>
+    <BrowserRouter>
+      <div className="size-full">
+        <Routes>
+          <Route path="/" element={<LandingPage onGetStarted={handleGetStarted} />} />
+          {/* <Route path="/auth" element={<AuthPage />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 

0ae7cb121289ff2bad7e9150ed46f66a1b7b8aad
chewkei_merck <keith.zikai.chew@msd.com>
2025-10-21 16:06:07 +0800

    include trips router in main router

--- backend/src/router.py
+++ backend/src/router.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 from src.groups.router import router as groups_router
+from src.trips.router import router as trips_router
 from src.users.router import router as users_router
 
 router = APIRouter()
 
 router.include_router(users_router)
 router.include_router(groups_router)
+router.include_router(trips_router)
 
 
 @router.get("/health")
